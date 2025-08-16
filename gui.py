835c7df2d222eee4261759eec54df351622aecdc
Z33shan-Aamir <zeeshanaamir29@gmail.com>
2025-08-16 13:56:13 +0500

    switching to linux

---
+++ gui.py
@@ -0,0 +1,40 @@
+import customtkinter as ttk
+from customtkinter import CTkLabel as Label
+
+APP_NAME= "Game Addiction Cure"
+def main():
+    
+    # window.title('Grid')
+    # window.geometry('600x400')
+
+    # label1 = ttk.CTkLabel(window, text = 'Label 1', fg_color = 'red')
+    # label2 = Label(window, text = 'Label 2', fg_color = 'blue')
+    # label3 = Label(window, text = 'Label 3', fg_color = 'green')
+    # label4 = Label(window, text = 'Label 4', fg_color = 'yellow')
+    # button1 = ttk.CTkButton(window, text = 'CTKButton 1')
+    # button2 = ttk.CTkButton(window, text = 'CTKButton 2')
+    # entry = ttk.CTkEntry(window)
+    
+    # window.columnconfigure(0, weight=1)
+    # window.columnconfigure(1, weight=1)
+    # window.columnconfigure(2, weight=1)
+    # window.columnconfigure(3, weight=10)
+    
+    # window.rowconfigure(0, weight=1)
+    # window.rowconfigure(1, weight=1)
+    # window.rowconfigure(2, weight=1)
+    # window.rowconfigure(3, weight=3)
+    
+    
+    # label1.grid(row=0, column=0, sticky="senw", pady=10, padx=10)
+    # label2.grid(row=0, column=1, sticky= "nsew", pady=10, padx=10)
+    # label3.grid(row=0, column=2, sticky= "nsew", pady=10, padx=10)
+    # label4.grid(row=0, column=3, sticky= "nsew", pady=10, padx=10)
+    
+    
+    
+    window = ttk.CTk()
+    
+    
+    window.mainloop()
+main()
